49fea69223662b7e7e7fb82b1ec91612b0175774
Trevor Manz <trevor.j.manz@gmail.com>
2024-06-19 13:08:43 -0400

    feat: Enumerate formatting for each data type

--- src/ipytable/widget.js
+++ src/ipytable/widget.js
@@ -1,17 +1,12 @@
 // @deno-types="npm:htl"
 import { html } from "https://esm.sh/htl@0.3.1";
-// @deno-types="npm:apache-arrow"
+// @deno-types="npm:apache-arrow@16.1.0"
 import * as arrow from "https://esm.sh/apache-arrow@16.1.0";
-// @deno-types="npm:temporal-polyfill"
-import { Temporal } from "https://esm.sh/temporal-polyfill@0.2.5";
-
-/**
- * @typedef Model
- * @prop {DataView} _ipc
- */
+// @deno-types="npm:@js-temporal/polyfill"
+import { Temporal } from "https://esm.sh/@js-temporal/polyfill@0.4.4";
 
 export default {
-	/** @type {import("npm:@anywidget/types").Render<Model>} */
+	/** @type {import("npm:@anywidget/types").Render<{ _ipc: DataView }>} */
 	render({ model, el }) {
 		let ipc = model.get("_ipc");
 		let table = arrow.tableFromIPC(ipc);
@@ -23,121 +18,6 @@ export default {
 // Lib
 
 /**
- * @param {unknown} date
- * @returns {string}
- */
-function formatDate(date) {
-	if (!(date instanceof Date)) {
-		return "Invalid Date";
-	}
-	return date.toISOString().split("T")[0];
-}
-
-/**
- * @param {number} msSinceEpoch
- * @returns {string}
- */
-function formatTimestamp(msSinceEpoch) {
-	return Temporal.Instant.fromEpochMilliseconds(msSinceEpoch)
-		.toZonedDateTimeISO("UTC")
-		.toPlainDateTime()
-		.toString();
-}
-
-/**
- * @param {unknown} value
- * @returns {value is undefined | null}
- */
-function undefinedOrNull(value) {
-	return value === undefined || value === null;
-}
-
-/**
- * @param {number} value
- * @returns {string}
- */
-function formatNumber(value) {
-	if (undefinedOrNull(value) || Number.isNaN(value)) {
-		return stringify(value);
-	}
-	return value === 0 ? "0" : value.toLocaleString("en"); // handle negative zero
-}
-
-/**
- * @param {any} x
- * @returns {string}
- */
-function stringify(x) {
-	return `${x}`;
-}
-
-/**
- * @param {import("npm:apache-arrow").DataType} type
- * @returns {type is arrow.Int | arrow.Float | arrow.Decimal}
- */
-function isNumberDataType(type) {
-	return (
-		arrow.DataType.isFloat(type) ||
-		arrow.DataType.isInt(type) ||
-		arrow.DataType.isDecimal(type)
-	);
-}
-
-/**
- * @param {import("npm:apache-arrow").DataType} type
- * @returns {(value: any) => string}
- */
-function formatterForType(type) {
-	if (isNumberDataType(type)) return formatNumber;
-	if (arrow.DataType.isTimestamp(type)) return formatTimestamp;
-	if (arrow.DataType.isDate(type)) return formatDate;
-	return stringify;
-}
-
-/**
- * @param {import("npm:apache-arrow").Table} table
- */
-function formatof(table) {
-	/** @type {Record<string, (value: any) => string>} */
-	const format = Object.create(null);
-	for (const field of table.schema.fields) {
-		format[field.name] = formatterForType(field.type);
-	}
-	return format;
-}
-
-/**
- * @param {import("npm:apache-arrow").Table} table
- * @returns {Record<string, "number" | "date">}
- */
-function classof(table) {
-	/** @type {Record<string, "number" | "date">} */
-	const classes = Object.create(null);
-	for (const field of table.schema.fields) {
-		if (isNumberDataType(field.type)) {
-			classes[field.name] = "number";
-		}
-		if (
-			arrow.DataType.isDate(field.type) ||
-			arrow.DataType.isTimestamp(field.type)
-		) {
-			classes[field.name] = "date";
-		}
-	}
-	return classes;
-}
-
-/** @param {arrow.DataType} type */
-function formatDataType(type) {
-	const dataTypeString = type.toString();
-	const overrides = {
-		"Timestamp<NANOSECOND>": "Timestamp<NS>",
-	};
-	// @ts-expect-error - TS doesn't understand that overrides is a subset of DataType
-	return overrides[dataTypeString] ?? dataTypeString;
-}
-
-/**
  * @param {import("npm:apache-arrow").Field} field
  * @param {string} width
  */
@@ -146,19 +26,33 @@ function thcol(field, width) {
 	return html.fragment`<th title=${field.name} style=${{ width }}>
 	<div style=${{ display: "flex",  flexDirection: "column", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}>
 		<span style=${{ marginBottom: "5px" }}>${field.name}</span>
-		<span class="gray" style=${{ fontWeight: 400, fontSize: "12px" }}>${formatDataType(field.type)}</span>
+		<span class="gray" style=${{ fontWeight: 400, fontSize: "12px" }}>${formatDataTypeName(field.type)}</span>
 	</div>
 </th>`;
 }
 
-class ArrowTable extends HTMLElement {
+class _HTMLElement {
+	/** @type {HTMLElement} */
+	#root;
+
+	constructor() {
+		this.#root = document.createElement("div");
+		/** @type {ShadowRoot} */
+		this.shadowRoot = this.#root.attachShadow({ mode: "open" });
+	}
+
+	node() {
+		return this.#root;
+	}
+}
+
+class ArrowDataTable extends _HTMLElement {
 	/** @type {import("npm:apache-arrow").Table} */
 	#table;
 
 	/** @param {import("npm:apache-arrow").Table} table */
 	constructor(table) {
 		super();
-		this.attachShadow({ mode: "open" });
 		this.#table = table;
 	}
 
@@ -176,6 +70,7 @@ class ArrowTable extends HTMLElement {
 		let columnWidth = "150px";
 		let headerHeight = "50px";
 		let maxHeight = `${(rows + 1) * rowHeight - 1}px`;
+		/** @type {HTMLDivElement} */
 		let root = html`<div class="ipytable" style=${{ maxHeight }}>`;
 
 		let cols = table.schema.fields.map((field) => field.name);
@@ -264,10 +159,7 @@ class ArrowTable extends HTMLElement {
 					let col = cols[j];
 					/** @type {string} */
 					let stringified = format[col](d[col]);
-					if (
-						stringified === "null" || stringified === "undefined" ||
-						stringified === "NaN"
-					) {
+					if (shouldGrayoutValue(stringified)) {
 						td.classList.add("gray");
 					}
 					let value = document.createTextNode(stringified);
@@ -277,48 +169,84 @@ class ArrowTable extends HTMLElement {
 			tbody.append(itr);
 		}
 
-		root.addEventListener("scroll", () => {
-			if (
-				root.scrollHeight - root.scrollTop < rows * rowHeight * 1.5 &&
-				n < minlengthof(n + 1)
-			) {
-				appendRows(n, n = minlengthof(n + rows));
-			}
-		});
+		// scroll behavior
+		{
+			root.addEventListener("scroll", (event) => {
+				event.stopPropagation();
+				if (
+					root.scrollHeight - root.scrollTop <
+						rows * rowHeight * 1.5 &&
+					n < minlengthof(n + 1)
+				) {
+					appendRows(n, n = minlengthof(n + rows));
+				}
+			});
+		}
 
-		{ // highlight on hover
-			root.addEventListener(
-				"mouseover",
-				(/** @type {MouseEvent} */ event) => {
-					if (event.target?.tagName === "TD") {
-						const cell = event.target;
-						const row = cell.parentNode;
-						if (
-							row.firstChild !== cell &&
-							cell !== row.children[cols.length + 1]
-						) {
-							cell.style.border = "1px solid var(--moon-gray)";
-						}
-						row.style.backgroundColor = "var(--light-silver)";
-					}
-				},
-			);
-			root.addEventListener(
-				"mouseout",
-				(/** @type {MouseEvent} */ event) => {
-					if (event.target?.tagName === "TD") {
-						const cell = event.target;
-						cell.style.removeProperty("border");
-						const row = cell.parentNode;
-						row.style.removeProperty("background-color");
-					}
-				},
-			);
+		// highlight on hover
+		{
+			root.addEventListener("mouseover", (event) => {
+				if (
+					isTableCellElement(event.target) &&
+					isTableRowElement(event.target.parentNode)
+				) {
+					const cell = event.target;
+					const row = event.target.parentNode;
+					highlight(cell, row);
+				}
+			});
+			root.addEventListener("mouseout", (event) => {
+				if (
+					isTableCellElement(event.target) &&
+					isTableRowElement(event.target.parentNode)
+				) {
+					const cell = event.target;
+					const row = event.target.parentNode;
+					removeHighlight(cell, row);
+				}
+			});
 		}
 
 		appendRows(0, n = minlengthof(rows * 2));
 		this.shadowRoot?.appendChild(root);
 	}
+}
+
+/**
+ * @param {HTMLTableCellElement} cell
+ * @param {HTMLTableRowElement} row
+ */
+function highlight(cell, row) {
+	if (row.firstChild !== cell && cell !== row.lastElementChild) {
+		cell.style.border = "1px solid var(--moon-gray)";
+	}
+	row.style.backgroundColor = "var(--light-silver)";
+}
+
+/**
+ * @param {HTMLTableCellElement} cell
+ * @param {HTMLTableRowElement} row
+ */
+function removeHighlight(cell, row) {
+	cell.style.removeProperty("border");
+	row.style.removeProperty("background-color");
+}
+
+/**
+ * @param {unknown} node
+ * @returns {node is HTMLTableDataCellElement}
+ */
+function isTableCellElement(node) {
+	// @ts-expect-error - tagName is not defined on unknown
+	return node?.tagName === "TD";
+}
+
+/**
+ * @param {unknown} node
+ * @returns {node is HTMLTableRowElement}
+ */
+function isTableRowElement(node) {
+	return node instanceof HTMLTableRowElement;
 }
 
 const STYLES = /* @css */ `\
@@ -446,14 +374,289 @@ function assert(condition, message) {
 
 /**
  * @param {arrow.Table} data
- * @returns {ArrowTable}
+ * @returns {HTMLElement}
  */
 function createArrowDataTable(data) {
-	if (!customElements.get("arrow-table")) {
-		customElements.define("arrow-table", ArrowTable);
+	let table = new ArrowDataTable(data);
+	table.connectedCallback();
+	return table.node();
+}
+
+/**
+ * @param {import("npm:apache-arrow").Table} table
+ */
+function formatof(table) {
+	/** @type {Record<string, (value: any) => string>} */
+	const format = Object.create(null);
+	for (const field of table.schema.fields) {
+		format[field.name] = formatterForDataTypeValue(field.type);
 	}
-	let AT = customElements.get("arrow-table");
-	assert(AT, "arrow-table not defined");
-	// @ts-expect-error - ArrowTable is a custom element
-	return new AT(data);
+	return format;
+}
+
+/**
+ * @param {import("npm:apache-arrow").Table} table
+ * @returns {Record<string, "number" | "date">}
+ */
+function classof(table) {
+	/** @type {Record<string, "number" | "date">} */
+	const classes = Object.create(null);
+	for (const field of table.schema.fields) {
+		if (
+			arrow.DataType.isInt(field.type) ||
+			arrow.DataType.isFloat(field.type)
+		) {
+			classes[field.name] = "number";
+		}
+		if (
+			arrow.DataType.isDate(field.type) ||
+			arrow.DataType.isTimestamp(field.type)
+		) {
+			classes[field.name] = "date";
+		}
+	}
+	return classes;
+}
+
+/**
+ * A utility function to create a formatter for a given data type.
+ *
+ * The datatype is only used for type inference to ensure that the formatter is
+ * correctly typed.
+ *
+ * @template TValue
+ *
+ * @param {TValue} _arrowDataTypeValue
+ * @param {(value: TValue) => string} format
+ *
+ * @returns {(value: TValue | null | undefined) => string}
+ */
+function fmt(_arrowDataTypeValue, format, log = false) {
+	return (value) => {
+		if (log) console.log(value);
+		if (value === undefined || value === null) {
+			return stringify(value);
+		}
+		return format(value);
+	};
+}
+
+/**
+ * @param {any} x
+ * @returns {string}
+ */
+function stringify(x) {
+	return `${x}`;
+}
+
+/** @param {arrow.DataType} type */
+function formatDataTypeName(type) {
+	// special case some types
+	if (arrow.DataType.isLargeBinary(type)) return "large binary";
+	if (arrow.DataType.isLargeUtf8(type)) return "large utf8";
+	// otherwise, just stringify and lowercase
+	return type
+		.toString()
+		.toLowerCase()
+		.replace("<second>", "[s]")
+		.replace("<millisecond>", "[ms]")
+		.replace("<microsecond>", "[µs]")
+		.replace("<nanosecond>", "[ns]")
+		.replace("<day>", "[day]")
+		.replace("dictionary<", "dict<");
+}
+
+/**
+ * @param {arrow.DataType} type
+ * @returns {(value: any) => string}
+ */
+function formatterForDataTypeValue(type) {
+	if (arrow.DataType.isNull(type)) {
+		return fmt(type.TValue, stringify);
+	}
+
+	if (
+		arrow.DataType.isInt(type) ||
+		arrow.DataType.isFloat(type)
+	) {
+		return fmt(type.TValue, (value) => {
+			if (Number.isNaN(value)) return "NaN";
+			return value === 0 ? "0" : value.toLocaleString("en"); // handle negative zero
+		});
+	}
+
+	if (
+		arrow.DataType.isBinary(type) ||
+		arrow.DataType.isFixedSizeBinary(type) ||
+		arrow.DataType.isLargeBinary(type)
+	) {
+		return fmt(type.TValue, (bytes) => {
+			let maxlen = 32;
+			let result = "b'";
+			for (let i = 0; i < Math.min(bytes.length, maxlen); i++) {
+				const byte = bytes[i];
+				if (byte >= 32 && byte <= 126) {
+					// ASCII printable characters range from 32 (space) to 126 (~)
+					result += String.fromCharCode(byte);
+				} else {
+					result += "\\x" + ("00" + byte.toString(16)).slice(-2);
+				}
+			}
+			if (bytes.length > maxlen) result += "...";
+			result += "'";
+			return result;
+		});
+	}
+
+	if (arrow.DataType.isUtf8(type) || arrow.DataType.isLargeUtf8(type)) {
+		return fmt(type.TValue, (text) => text);
+	}
+
+	if (arrow.DataType.isBool(type)) {
+		return fmt(type.TValue, stringify);
+	}
+
+	if (arrow.DataType.isDecimal(type)) {
+		return fmt(type.TValue, () => "TODO");
+	}
+
+	if (arrow.DataType.isDate(type)) {
+		return fmt(type.TValue, (ms) => {
+			// Always returns value in milliseconds
+			// https://github.com/apache/arrow/blob/89d6354068c11a66fcec2f34d0414daca327e2e0/js/src/visitor/get.ts#L167-L171
+			return Temporal.Instant
+				.fromEpochMilliseconds(ms)
+				.toZonedDateTimeISO("UTC")
+				.toPlainDate()
+				.toString();
+		});
+	}
+
+	if (arrow.DataType.isTime(type)) {
+		return fmt(type.TValue, (ms) => {
+			return instantFromTimeUnit(ms, type.unit)
+				.toZonedDateTimeISO("UTC")
+				.toPlainTime()
+				.toString();
+		});
+	}
+
+	if (arrow.DataType.isTimestamp(type)) {
+		return fmt(type.TValue, (ms) => {
+			// Always returns value in milliseconds
+			// https://github.com/apache/arrow/blob/89d6354068c11a66fcec2f34d0414daca327e2e0/js/src/visitor/get.ts#L173-L190
+			return Temporal.Instant
+				.fromEpochMilliseconds(ms)
+				.toZonedDateTimeISO("UTC")
+				.toPlainDateTime()
+				.toString();
+		});
+	}
+
+	if (arrow.DataType.isInterval(type)) {
+		return fmt(type.TValue, (_value) => {
+			return "TODO";
+		});
+	}
+
+	if (arrow.DataType.isDuration(type)) {
+		return fmt(type.TValue, (bigintValue) => {
+			// https://tc39.es/proposal-temporal/docs/duration.html#toString
+			return durationFromTimeUnit(bigintValue, type.unit).toString();
+		});
+	}
+
+	if (arrow.DataType.isList(type)) {
+		return fmt(type.TValue, (value) => {
+			// TODO: Some recursive formatting?
+			return value.toString();
+		});
+	}
+
+	if (arrow.DataType.isStruct(type)) {
+		return fmt(type.TValue, (value) => {
+			// TODO: Some recursive formatting?
+			return value.toString();
+		});
+	}
+
+	if (arrow.DataType.isUnion(type)) {
+		return fmt(type.TValue, (_value) => {
+			return "TODO";
+		});
+	}
+	if (arrow.DataType.isMap(type)) {
+		return fmt(type.TValue, (_value) => {
+			return "TODO";
+		});
+	}
+
+	if (arrow.DataType.isDictionary(type)) {
+		let formatter = formatterForDataTypeValue(type.dictionary);
+		return fmt(type.TValue, formatter);
+	}
+
+	return () => `Unsupported type: ${type}`;
+}
+
+/**
+ * @param {number | bigint} value
+ * @param {arrow.TimeUnit} unit
+ */
+function instantFromTimeUnit(value, unit) {
+	if (unit === arrow.TimeUnit.SECOND) {
+		if (typeof value === "bigint") value = Number(value);
+		return Temporal.Instant.fromEpochSeconds(value);
+	}
+	if (unit === arrow.TimeUnit.MILLISECOND) {
+		if (typeof value === "bigint") value = Number(value);
+		return Temporal.Instant.fromEpochMilliseconds(value);
+	}
+	if (unit === arrow.TimeUnit.MICROSECOND) {
+		if (typeof value === "number") value = BigInt(value);
+		return Temporal.Instant.fromEpochMicroseconds(value);
+	}
+	if (unit === arrow.TimeUnit.NANOSECOND) {
+		if (typeof value === "number") value = BigInt(value);
+		return Temporal.Instant.fromEpochNanoseconds(value);
+	}
+	throw new Error("Invalid TimeUnit");
+}
+
+/**
+ * @param {number | bigint} value
+ * @param {arrow.TimeUnit} unit
+ */
+function durationFromTimeUnit(value, unit) {
+	// TODO: Temporal.Duration polyfill only supports number not bigint
+	value = Number(value);
+	if (unit === arrow.TimeUnit.SECOND) {
+		return Temporal.Duration.from({ seconds: value });
+	}
+	if (unit === arrow.TimeUnit.MILLISECOND) {
+		return Temporal.Duration.from({ milliseconds: value });
+	}
+	if (unit === arrow.TimeUnit.MICROSECOND) {
+		return Temporal.Duration.from({ microseconds: value });
+	}
+	if (unit === arrow.TimeUnit.NANOSECOND) {
+		return Temporal.Duration.from({ nanoseconds: value });
+	}
+	throw new Error("Invalid TimeUnit");
+}
+
+/** @param {string} value */
+function shouldGrayoutValue(value) {
+	return (
+		value === "null" ||
+		value === "undefined" ||
+		value === "NaN" ||
+		value === "TODO"
+	);
+}
+
+/**
+ * @param {Uint8Array} bytes
+ */
+function bytesToHex(bytes, limit = 32) {
 }
