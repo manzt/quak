04578207a3f848f2e0f3cc9887afac273ebf4807
Trevor Manz <trevor.j.manz@gmail.com>
2024-06-20 14:25:22 -0400

    Add ruff

--- src/ipytable/__init__.py
+++ src/ipytable/__init__.py
@@ -1,28 +1,6 @@
-import importlib.metadata
-import pathlib
+"""An anywidget for displaying tabular data in a table."""
 
-import anywidget
-import anywidget.experimental
-import traitlets
-import duckdb
-from ._util import arrow_table_from_dataframe_protocol, arrow_to_ipc
+from ._version import __version__
+from ._widget import Widget
 
-__version__ = importlib.metadata.version("ipytable")
-
-
-class Widget(anywidget.AnyWidget):
-    _esm = pathlib.Path(__file__).parent / "widget.js"
-    _query = traitlets.Dict().tag(sync=True)
-
-    def __init__(self, df):
-        super().__init__(_query={"orderby": []})
-        con = duckdb.connect(":memory:")
-        con.register("df", arrow_table_from_dataframe_protocol(df))
-        self._con = con
-
-    @anywidget.experimental.command
-    def _run_query(self, msg: dict, buffers: list[bytes]):
-        print(msg["sql"])
-        result = self._con.execute(msg["sql"])
-        ipc = arrow_to_ipc(result.arrow())
-        return None, [ipc.tobytes()]
+__all__ = ["Widget", "__version__"]
