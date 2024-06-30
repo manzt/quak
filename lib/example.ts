2081dd0f5b3b5899d2dff397a5197c2ffface9af
Trevor Manz <trevor.j.manz@gmail.com>
2024-06-29 20:57:21 -0400

    feat: Get vite stuff working

--- lib/example.ts
+++ lib/example.ts
@@ -3,33 +3,52 @@ import * as msql from "@uwdata/mosaic-sql";
 
 import { DataTable } from "./clients/DataTable.ts";
 
-let coordinator = new mc.Coordinator();
-coordinator.databaseConnector(mc.wasmConnector());
-let logger = coordinator.logger();
+let base = new URL(
+	"https://raw.githubusercontent.com/uwdata/mosaic/main/data/",
+);
+let table: keyof typeof datasets = "metros";
+let datasets = {
+	athletes: new URL("athletes.csv", base).href,
+	unemployment: new URL("us-county-unemployment.csv", base).href,
+	metros: new URL("metros.csv", base).href,
+} as const;
 
-console.log(mc.wasmConnector());
+let coordinator = new mc.Coordinator();
+let logger = coordinator.logger();
+let connector = mc.wasmConnector();
+
+// Wrap the DuckDB-WASM connector with some logging
+coordinator.databaseConnector({
+	async query(query: msql.Query) {
+		logger.group(`query`);
+		logger.log("query", query);
+		let result = await connector.query(query);
+		logger.log("result", result);
+		logger.groupEnd(`query`);
+		return result;
+	},
+});
 
 await coordinator.exec([
-	msql.loadCSV(
-		"athletes",
-		"https://raw.githubusercontent.com/uwdata/mosaic/8e63149753e7d6ca30274c032a04744e14df2fd6/data/athletes.csv",
-	),
+	// @deno-fmt-ignore
+	msql.loadCSV(table, datasets[table]),
 ]);
 
 // TODO: This should be a helper function
 let empty = await coordinator.query(
 	msql.Query
-		.from("athletes")
+		.from(table)
 		.select(["*"])
 		.limit(0)
 		.toString(),
 );
-console.log(empty.schema);
 
-let table = new DataTable({
-	table: "athletes",
+let datatable = new DataTable({
+	table,
 	schema: empty.schema,
 	filterBy: mc.Selection.crossfilter(),
+	height: 500,
 });
 
-document.body.appendChild(table.node());
+coordinator.connect(datatable);
+document.body.appendChild(datatable.node());
