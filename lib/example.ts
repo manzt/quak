1c26b567e3986c02645eb5d7abccee60d84a4ee7
Trevor Manz <trevor.j.manz@gmail.com>
2024-07-02 09:43:07 -0400

    chore: Setup Vite HMR reload for DataTable

--- lib/example.ts
+++ lib/example.ts
@@ -1,12 +1,13 @@
+/// <reference types="npm:vite/client" />
 import * as mc from "@uwdata/mosaic-core";
 import * as msql from "@uwdata/mosaic-sql";
 
 import { DataTable } from "./clients/DataTable.ts";
 
+let table: keyof typeof datasets = "athletes";
 let base = new URL(
 	"https://raw.githubusercontent.com/uwdata/mosaic/main/data/",
 );
-let table: keyof typeof datasets = "athletes";
 let datasets = {
 	athletes: new URL("athletes.csv", base).href,
 	unemployment: new URL("us-county-unemployment.csv", base).href,
@@ -14,10 +15,9 @@ let datasets = {
 } as const;
 
 let coordinator = new mc.Coordinator();
-let logger = coordinator.logger();
-let connector = mc.wasmConnector();
-
-// Wrap the DuckDB-WASM connector with some logging
+let logger = coordinator.logger(voidLogger());
+// let logger = coordinator.logger();
+let connector = mc.wasmConnector(); // Wrap the DuckDB-WASM connector with some logging
 coordinator.databaseConnector({
 	async query(query: msql.Query) {
 		logger.group(`query`);
@@ -52,3 +52,28 @@ let datatable = new DataTable({
 
 coordinator.connect(datatable);
 document.body.appendChild(datatable.node());
+
+// A Vite-specific feature that allows hot-reloading of modules. This way we
+// don't need to reload DuckDB or the data when we make changes to the
+// DataTable client.
+//
+// @see https://vitejs.dev/guide/api-hmr
+// @ts-expect-error - import.meta.hot not coming from Deno
+import.meta.hot?.accept("./clients/DataTable.ts", (mod) => {
+	coordinator.disconnect(datatable);
+	document.body.removeChild(datatable.node());
+	datatable = new mod.DataTable({
+		table,
+		schema: empty.schema,
+		filterBy: mc.Selection.crossfilter(),
+		height: 500,
+	});
+	coordinator.connect(datatable);
+	document.body.appendChild(datatable.node());
+});
+
+function voidLogger() {
+	return Object.fromEntries(
+		Object.keys(console).map((key) => [key, () => {}]),
+	);
+}
